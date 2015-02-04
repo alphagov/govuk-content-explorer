4489931b83bf18feff7fea78547cb9c00267c4d3
Richard Boulton <richard.boulton@digital.cabinet-office.gov.uk>
2015-02-04 07:41:16 +0000

    Prepare for running on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn explorer:app --log-file=-
--- explorer/search.py
+++ explorer/search.py
@@ -1,7 +1,7 @@
 import requests
 
-GOVUK_SEARCH_API="http://www.dev.gov.uk/api/search.json"
-#GOVUK_SEARCH_API="https://www.gov.uk/api/search.json"
+#GOVUK_SEARCH_API="http://www.dev.gov.uk/api/search.json"
+GOVUK_SEARCH_API="https://www.gov.uk/api/search.json"
 
 def perform_search(**params):
     r = requests.get(GOVUK_SEARCH_API, params=params).json()
