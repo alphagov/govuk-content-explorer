0efee2b3bbedd5f0c2e8d1233660d19b12ec39d5
Richard Boulton <richard.boulton@digital.cabinet-office.gov.uk>
2015-02-11 14:08:41 +0000

    Allow controlling the search api endpoint using environment variables

--- explorer/config.py
+++ explorer/config.py
@@ -8,3 +8,15 @@ elif env == "dev":
     GOVUK_SEARCH_API = "http://www.dev.gov.uk/api/search.json"
 else:
     raise RuntimeError("Unknown value for ENV environment variable: %r" % env)
+
+gsi = os.environ.get("GOVUK_SEARCH_API")
+if gsi:
+    GOVUK_SEARCH_API = gsi
+
+auth_password = os.environ.get("AUTH_PASSWORD")
+auth_username = os.environ.get("AUTH_USERNAME")
+
+if auth_password:
+    AUTH = (auth_username, auth_password)
+else:
+    AUTH = None
--- explorer/search.py
+++ explorer/search.py
@@ -3,5 +3,10 @@ import requests
 
 
 def perform_search(**params):
-    response = requests.get(config.GOVUK_SEARCH_API, params=params)
+    response = requests.get(
+        config.GOVUK_SEARCH_API,
+        params=params,
+        auth=config.AUTH,
+        verify=False,
+    )
     return response.json()
